2c63bdd76efbc6a679d4109d82647ee5e94cb0c0
Martin Page <martin.page@tsl.ac.uk>
2014-07-09 12:27:59 +0100

    fasta lib start

--- controllers/GenomesController.js
+++ controllers/GenomesController.js
@@ -1,4 +1,5 @@
 var Genome = require('../models/Genome');
+var Fasta = require('fasta');
 module.exports.controller = function (app) {
 
     app.get('/genomes', function (req, res) {
@@ -12,6 +13,12 @@ module.exports.controller = function (app) {
 
     app.get('/genomes/new', function (req, res) {
         res.render('genomes/new')
+    });
+
+    app.post('/genomes/add', function(req, res){
+
+        Fasta.read();
+
     });
 
     app.get('/genomes/show', function (req, res) {
