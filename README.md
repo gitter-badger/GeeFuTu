376871cd8b66465545ff107c4fab38a6d50e9be5
Martin Page <wookoouk@gmail.com>
2014-09-24 11:28:43 +0100

    Added roadmap; adding passport support;

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 GeeFuTu
 =======
-jee - foo - too(l)
+
+GeeFuTu is a site for sharing and updating genomics data.
+
+### Roadmap
+
+#### Dec 2014
+
+* Hosted service available.
+* Editing inside BioDalliance.
+* Private projects with custom permissions.
+* Public projects.
+* GFF, BAM, VCF, BigWig and BED support.
+* BioJS visualisation.
+* Tutorial.
+* Full test coverage.
+* Remote logging.
+* Documentation.
+* Cross OS/Browser support (Safari, Chrome, Firefox, Opera, IE9 and mobile browsers (Android, Chrome Mobile, iOS Safari).
+
+
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var passport = require('passport');
+LocalStrategy = require('passport-local').Strategy
+var session = require('express-session');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var multer  = require('multer');
@@ -14,7 +17,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ dest: './uploads/'}));
 
 
-app.use(methodOverride()); 					// simulate DELETE and PUT
+app.use(passport.initialize());
+app.use(passport.session());
+
+//app.use(methodOverride()); 					// simulate DELETE and PUT
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
